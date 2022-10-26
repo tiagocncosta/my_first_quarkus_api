ff44a4243dcf1bcd385e1f49e51e06f0b57a744e
tiagocncosta <tiagocncosta@gmail.com>
2022-10-26 10:34:54 +0100

    first commit
    simple hello world restApi with quarkus

---
+++ first_quarkus_api/src/test/java/quarkus/project/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package quarkus.project;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
