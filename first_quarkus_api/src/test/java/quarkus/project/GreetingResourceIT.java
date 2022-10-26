8830dde84b3ead5bec16a5eede028331f30f7232
tiagocncosta <tiagocncosta@gmail.com>
2022-10-26 15:54:24 +0100

    quarkus-native
    Quarkus native api with GraalVM

--- first_quarkus_api/src/test/java/quarkus/project/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package quarkus.project;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
